a48e9e8f6bd254b457cae94d3b6724887734cd20
Sughosh <sughoshathreya1@gmail.com>
2025-03-16 18:09:33 +0530

    second

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Logger;
+
 @SpringBootTest
 class DemoApplicationTests {
+	public static Logger logger = Logger.getLogger(DemoApplicationTests.class.getName());
 		@Test
+
 		void contextLoads() {
 			assert(true);
+			logger.info("Test case executing");
 		}
 
 
